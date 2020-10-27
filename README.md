c27435160c93be0c268ae787ef9fb4d35d44a3b8
Jad Issa <jad.issa@protonmail.com>
2020-10-27 23:31:13 +0200

    First assignment submission.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CMPS 252 HW4.2
+
+Please note that I am running on Linux, so I had to change some parameters for
+the execution of the program just so that it runs.
--- src/cmps252/HW4_2/FileParser.java
+++ src/cmps252/HW4_2/FileParser.java
@@ -1,5 +1,53 @@
 package cmps252.HW4_2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
 public class FileParser {
-	//TODO add your code here
+	public static ArrayList<Customer> getCustomers(String fileName) throws FileNotFoundException {
+		ArrayList<Customer> result = new ArrayList<Customer>();
+		//File file = new File(fileName);
+		//BufferedReader br = new BufferedReader(new FileReader(file)); 
+		String currentDirectory = new File("").getAbsolutePath();
+		BufferedReader br = new BufferedReader(new FileReader(currentDirectory + "/" + fileName));
+		String entry = null;
+		try {
+			br.readLine();
+			entry = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (entry != null) {
+			String[] fields = entry.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+			for(int i = 0; i < fields.length; i++) {
+				fields[i] = fields[i].replace("\"", "");
+			}
+			result.add(new Customer(fields[0],
+									fields[1],
+									fields[2],
+									fields[3],
+									fields[4],
+									fields[5],
+									fields[6],
+									fields[7],
+									fields[8],
+									fields[9],
+									fields[10],
+									fields[11]));
+			try {
+				entry = br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				break;
+			}
+		}
+		return result;
+	}
 }
